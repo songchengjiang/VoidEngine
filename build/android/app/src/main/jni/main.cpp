f79f34487ca79105215f8af9fc46a12451ae60de
songchengjiang <moses_jc@sina.com>
2015-11-02 16:44:36 +0800

    add android_native_app_glue for pure c/c++ develop

---
+++ build/android/app/src/main/jni/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by songcheng.jiang on 2015/11/2.
+//
+
+#include "native_app_glue/android_native_app_glue.h"
+#include <android/log.h>
+
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "VoidEngine", __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "VoidEngine", __VA_ARGS__))
+
+void android_main(struct android_app* app){
+
+}
+
