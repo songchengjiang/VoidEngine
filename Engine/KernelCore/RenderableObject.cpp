8ff6b4905d98e785fe63c087788d4fb91d87b895
songchengjiang <moses_jc@sina.com>
2016-04-06 18:13:29 +0800

    fix a crash bug when has no materials

--- Engine/KernelCore/RenderableObject.cpp
+++ Engine/KernelCore/RenderableObject.cpp
@@ -25,6 +25,6 @@ void veRenderableObject::update(veNode *node, veSceneManager *sm)
 void veRenderableObject::render(veNode *node, veCamera *camera)
 {
 	if (!_isVisible) return;
-	if (_renderer.valid())
+	if (_renderer.valid() && _materials.valid())
 		_renderer->render(node, this, camera);
 }
